a8fc537a0a5ec554e39050cbe09f9a6b187df012
dduyanhhoang <dduyanhhoang@gmail.com>
2024-02-29 08:38:07 +0700

    Initial commit

--- Assignments and exams/20240229/main.py
+++ Assignments and exams/20240229/main.py
@@ -1,3 +1,24 @@
+import logging
+
+
+def sort_employee(e: dict) -> list:
+    return sorted(e.items(), key=lambda x: x[1][1], reverse=True)
+
+
+def get_emp_salary(prompt: str) -> float:
+    while True:
+        try:
+            salary = input(prompt)
+            try:
+                salary = float(salary)
+            except ValueError:
+                raise ValueError("Salary must be a number")
+            if salary < 0:
+                raise ValueError("Salary must be a positive number")
+            return salary
+        except ValueError as e:
+            logging.error(f"An error occurred while getting employee salary: {e}")
+            continue
 
 
 def get_employee(n: int) -> dict:
@@ -11,46 +32,42 @@ def get_employee(n: int) -> dict:
                 raise ValueError("Employee id already exists")
             
             name = input("Enter name: ")
-
-            salary = input("Enter salary: ")
-            try:
-                salary = float(salary)
-            except ValueError:
-                raise ValueError("Salary must be a number")
-            
-            if salary < 0:
-                raise ValueError("Salary must be a positive number")
+            salary = get_emp_salary("Enter salary: ")
             
             emp_num += 1
             employees[emp_code] = [name, salary]
         except ValueError as e:
-            print(f"An error occurred while getting employees: {e}")
+            logging.error(f"An error occurred while getting employees: {e}")
             continue
     
     return employees
 
 
-def sort_employee(e: dict) -> list:
-    return sorted(e.items(), key=lambda x: x[1][1], reverse=True)
-
-
 def main():
-    try:
-        num_employees = input("Enter the number of employees: ")
+    while True:
         try:
-            num_employees = int(num_employees)
-        except ValueError:
-            raise ValueError("Number of employees must be a number")
+            num_employees = input("Enter the number of employees: ")
+            try:
+                num_employees = int(num_employees)
+            except ValueError:
+                raise ValueError("Number of employees must be a number")
+            
+            if num_employees < 0:
+                raise ValueError("Number of employees must be a positive number")
 
-        employees = get_employee(num_employees)
+            employees = get_employee(num_employees)
 
-        sorted_employees = sort_employee(employees)
-        for _ in sorted_employees:
-            print(f"Employee: {_[0]}, Salary: {_[1][1]}, Name: {_[1][0]}")
-    except (KeyboardInterrupt, EOFError):
-        print("\nInterrupted by the user. Exiting.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            sorted_employees = sort_employee(employees)
+            for employee in sorted_employees:
+                print(f"Employee: {employee[0]}, Salary: {employee[1][1]}, Name: {employee[1][0]}")
+            
+            exit(0)
+        except (KeyboardInterrupt, EOFError):
+            print("\nInterrupted by the user. Exiting.")
+            exit(0)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            continue
 
 
 if __name__ == "__main__":
